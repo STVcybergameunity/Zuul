1c824dc650a3bcd8d2458f5fc3a37b58fc2e61f1
STVcybergameunity <cybergameunity@gmail.com>
2026-01-07 10:40:23 +0100

    Add Game field and Heal method to Player class
    Added a private Game field and a Heal method to reset health.

--- src/Player.cs
+++ src/Player.cs
@@ -1,13 +1,19 @@
 class Player
 {
+    private Game game;
     //fields
-    private int health;
+    public int health;
     // auto property
     public Room CurrentRoom { get; set; }
     // constructor
     public Player()
     {
         CurrentRoom = null;
+        health = 100;
+    }
+    //methods
+    void Heal()
+    {
         health = 100;
     }
 }
