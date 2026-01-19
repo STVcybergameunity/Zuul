6ed1a68571bb011aa5a91c6879d0d966af78621f
STVcybergameunity <cybergameunity@gmail.com>
2026-01-19 09:09:48 +0100

    Add comment to Item constructor
    Added a comment to the Item constructor for clarity.

--- src/Item.cs
+++ src/Item.cs
@@ -6,6 +6,8 @@ class Item
     public string Description { get; }
 
     //constructor
+
+    // Makes a item from the information given at Game
     public Item(int weight, string description)
     {
         Weight = weight;
